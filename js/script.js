7c7e36c71b76a58fce4e80978a6b134c533bec08
Muhammad Ali <Muhammad_Ali@workmail.com>
2024-01-03 23:58:29 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -53,18 +53,4 @@ window.onbeforeunload = function() {
 
 window.addEventListener('load', function () {
     dispalyGrids();
-    
-    // Request fullscreen
-    if (document.documentElement.requestFullscreen) {
-        document.documentElement.requestFullscreen();
-    } else if (document.documentElement.mozRequestFullScreen) { /* Firefox */
-        document.documentElement.mozRequestFullScreen();
-    } else if (document.documentElement.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
-        document.documentElement.webkitRequestFullscreen();
-    } else if (document.documentElement.msRequestFullscreen) { /* IE/Edge */
-        document.documentElement.msRequestFullscreen();
-    }
-
-    // Scroll to the bottom
-    window.scrollTo(0, document.body.scrollHeight);
 });
