79cc267e50f6fabc34fd1d96ea26ab659536baba
Muhammad Ali <muhammad_ali@workmail.com>
2024-01-27 12:05:46 +0500

    almost final

--- js/script.js
+++ js/script.js
@@ -65,8 +65,8 @@ window.onbeforeunload = function() {
     return "Data will be lost if you leave the page, are you sure?";
 };
 // generate a sudoku
-function generateSudoku() {
-    var sudokustring = sudoku.generate("easy");
+function generateSudoku(difficulty) {
+    var sudokustring = sudoku.generate(difficulty);
     console.log(sudokustring);
 
     var cells = document.getElementsByClassName("sudoku-cell");
@@ -77,9 +77,75 @@ function generateSudoku() {
             cells[i].classList.add("sudoku-cell--initial");
         }
     }
+    return sudokustring;
 }
+
+function solveSudoku(string){
+    var solvedsudokustring = sudoku.solve(string)
+    return solvedsudokustring
+}
+
+
+
+function checkSudoku() {
+    var cells = document.getElementsByClassName("sudoku-cell");
+    console.log(cells[10].textContent)
+    let currentString = "";
+    // create current list
+    [...cells].forEach(element => {
+        if (element.textContent == "") {
+            currentString += ".";
+        } else {
+            currentString += element.textContent;
+        }
+    });
+    console.log(currentString)
+    // solve current list
+    for (let i = 0; i < currentString.length; i++) {
+        if (currentString[i] !== ".") {
+            if (!cells[i].classList.contains("sudoku-cell--initial")) {
+                if (currentString[i] === solvedSudokuString[i]) {
+                    cells[i].style.backgroundColor = "#00ff3729";
+                    // wait for 5 seconds
+                    setTimeout(function() {
+                        cells[i].style.backgroundColor = "transparent";
+                    }, 5000);
+                }else {
+                    cells[i].style.backgroundColor = "#ff000026";
+                    setTimeout(function() {
+                        cells[i].style.backgroundColor = "transparent";
+                    }, 5000);
+                } 
+            }
+        }
+    }
+}
+
+var checkButton = document.querySelector("#check");
+checkButton.addEventListener("click", checkSudoku);
+var resetButton = document.getElementById("reset");
+resetButton.addEventListener("click", function(){
+    // Reload the page without triggering beforeunload
+    window.location.href = window.location.href;
+});
+
+var sudokuString;
+var solvedSudokuString;
+
+var solveButton = document.querySelector("#solve");
+solveButton.addEventListener("click", function(){
+    var cells = document.getElementsByClassName("sudoku-cell");
+    [...cells].forEach((element, i) => {
+        if (!element.classList.contains("sudoku-cell--initial")) {
+            element.textContent = solvedSudokuString[i];
+            console.log(element.id, element.textContent)
+        }
+    });
+});
+
 
 window.addEventListener('load', function () {
     dispalyGrids();
-    generateSudoku();
+    sudokuString = generateSudoku("hard");
+    solvedSudokuString = solveSudoku(sudokuString)
 });
